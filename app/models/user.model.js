5a335dd3116cfa844f7a095cdab94016d7d7ab8e
srikanta gowda <srikantagowda07@gmail.com>
2020-09-30 05:18:48 +0530

    user tbale model, holds user database info

---
+++ app/models/user.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+  const user = sequelize.define("user", {
+    user_id: {
+      type: Sequelize.STRING(25),
+      allowNull: false,
+      primaryKey: true,
+      // autoIncrement: true,
+    },
+    user_name: {
+      type: Sequelize.STRING,
+      // allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      // allowNull: false,
+    },
+  }, {
+    // disable the modification of table names; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true,
+  });
+
+  // Tutorial.associate = models => {
+  //   Tutorial.hasone(models.extra, {
+  //     onDelete: "cascade",
+  //     foreignKey: 'id',
+  //   })
+  // }
+  return user;
+};

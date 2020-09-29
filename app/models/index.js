dd9fffb055024268e3216ef7a8ba40de607b0517
srikanta gowda <srikantagowda07@gmail.com>
2020-09-30 05:16:09 +0530

    Index js file holds sequelize database connect with other models

---
+++ app/models/index.js
@@ -0,0 +1,34 @@
+const dbConfig = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+db.tutorials = require("./events.model.js")(sequelize, Sequelize)
+db.extras = require("./extra.model.js")(sequelize, Sequelize)
+
+db.citys = require("./city.model.js")(sequelize, Sequelize)
+db.movies = require("./movie.model.js")(sequelize, Sequelize)
+db.theatres = require("./theatre.model.js")(sequelize, Sequelize)
+db.users = require("./user.model.js")(sequelize, Sequelize)
+db.cityMovieJunctions = require("./city_movie.model.js")(sequelize, Sequelize)
+db.bookings = require("./booking.model.js")(sequelize, Sequelize)
+
+module.exports = db;

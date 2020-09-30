b55921c6b76eb1188de5ef32d1ee40458a1477a1
srikanta gowda <srikantagowda07@gmail.com>
2020-09-30 21:41:50 +0530

    added access-control-allow-origin

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  // origin: "http://localhost:8081"
+  "Access-Control-Allow-Origin": "*"
+
+
 };
 
 // createDatabasesTable();

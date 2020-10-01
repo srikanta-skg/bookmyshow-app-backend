c8d4db8dee36dc4cb62f520439d1fe1abe5f269f
srikanta gowda <srikantagowda07@gmail.com>
2020-10-01 13:38:38 +0530

    heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,22 +1,8 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "gowda1633",
-  DB: "bmssubmit",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-
 // module.exports = {
-//   HOST: "us-cdbr-east-02.cleardb.com",
-//   USER: "bc901833a6cc85",
-//   PASSWORD: "9f30f6e3",
-//   DB: "heroku_067921c158de4d4",
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "gowda1633",
+//   DB: "bmssubmit",
 //   dialect: "mysql",
 //   pool: {
 //     max: 5,
@@ -25,3 +11,17 @@ module.exports = {
 //     idle: 10000
 //   }
 // };
+
+module.exports = {
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "bc901833a6cc85",
+  PASSWORD: "9f30f6e3",
+  DB: "heroku_067921c158de4d4",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
